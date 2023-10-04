09d10d8bf9b323273bc68218f8b0f0c0f51c1695
BluBerryJay <arodrigues@student.neumont.edu>
2023-10-03 21:14:16 -0600

    Add project files.

---
+++ Source/Application/World01.cpp
@@ -0,0 +1,50 @@
+#include "World01.h"
+#include "Framework/Framework.h"
+#include "Renderer/Renderer.h"
+#include "Input/InputSystem.h"
+
+namespace nc
+{
+    bool World01::Initialize()
+    {
+        m_positions.push_back({ randomf(-1, 1), randomf(-1, 1) });
+        return true;
+    }
+
+    void World01::Shutdown()
+    {
+    }
+
+    void World01::Update(float dt)
+    {
+        m_angle += 90 * dt;
+        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
+        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? +dt : 0;
+        m_time += dt;
+    }
+
+    void World01::Draw(Renderer& renderer)
+    {
+        // pre-render
+        renderer.SetColor(0, 0, 0, 0);
+        renderer.BeginFrame();
+
+        // render
+        glPushMatrix();
+        glTranslatef(m_position.x, m_position.y, 0);
+        
+        glScalef((sin(m_time * 5) + 1) * 0.5f, 1, 1);
+        glBegin(GL_TRIANGLES);
+        glColor3f(1, 0, 0);
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0, 1, 0);
+        glVertex2f(0, 0.5f);
+        glColor3f(0, 0, 1);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        glPopMatrix();
+
+        // post-render
+        renderer.EndFrame();
+    }
+}
